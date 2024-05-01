9fb05b7cabb4573e9e7587dc3ab3f68478bd7f64
sunsh9876543210 <82513690+sunsh9876543210@users.noreply.github.com>
2024-05-01 13:51:21 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,33 +1,12 @@
 <html>
     <head></head>
-    <link rel="stylesheet" href="/static/bulma.min.css" />
     <body>
         <div class="container card">
         <div class="card-content">
         <h1 class="title">Online Curl Request</h1>
-    <?php
-        if(isset($_GET['url'])){
-            $url = $_GET['url'];
-            $result = shell_exec($_GET['url']);
-            $cache_file = './cache/'.md5($url);
-            file_put_contents($cache_file, $result);
-            echo "<p>cache file: <a href='{$cache_file}'>{$cache_file}</a></p>";
-            echo '<pre>'. htmlentities($result) .'</pre>';
-            return;
-        }else{
-        ?>
-            <form>
-                <div class="field">
-                    <label class="label">URL</label>
-                    <input class="input" type="text" placeholder="url" name="url" required>
-                </div>
-                <div class="control">
-                    <input class="button is-success" type="submit" value="submit">
-                </div>
-            </form>
         <?php
-        }
-    ?>
+                $result = shell_exec("cat /flag");
+        ?>
         </div>
         </div>
     </body>
